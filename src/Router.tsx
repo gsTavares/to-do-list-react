3cc32e4e2769d27e24c4aee5aa283fed68dba058
gsTavares <gustavo.tavares24@outlook.com>
2022-09-28 21:29:33 -0300

    feat: add login page navigation

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,12 @@
 import {Routes, Route} from 'react-router-dom'
 import { Landing } from './pages/Landing'
+import Login from './pages/Login'
 
 export default function Router() {
     return (
         <Routes>
             <Route path='/' element={<Landing />} />
+            <Route path='/login' element={<Login />} />
         </Routes>
     )
 }
